55c98a6039d9a5a6e7fc9efaa78422ff1d4854b1
HesamMirzaie <mirzaeeh08@gmail.com>
2024-08-09 21:51:50 +0330

    did with Object.groupBy

--- javascript/deep-clone.js
+++ javascript/deep-clone.js
@@ -1,11 +1,48 @@
-/**
- * Creates a deep copy of the arg. Remember that the arg can be nested with infinite levels.
- * Use case: List copy. Object copy.
- *
- * @param {JSON serializable value} arg Any JSON serializable value
- *
- * @returns An Literal exact copy of the arg
- */
-function deepClone(arg) {
-  // TODO: Implement here
+// /**
+//  * Creates a deep copy of the arg. Remember that the arg can be nested with infinite levels.
+//  * Use case: List copy. Object copy.
+//  *
+//  * @param {JSON serializable value} arg Any JSON serializable value
+//  *
+//  * @returns An Literal exact copy of the arg
+//  */
+// function deepClone(arg) {
+//   // TODO: Implement here
+//   return structuredClone(arg);
+// }
+
+// const arr = [
+//   {
+//     name: 'Hesam',
+//     toDoes: [
+//       { id: 1, task: 'Study', date: new Date() },
+//       { id: 2, task: 'Study', date: new Date() },
+//       { id: 3, task: 'Study', date: new Date() },
+//     ],
+//   },
+//   {
+//     name: 'Alex',
+//     toDoes: [
+//       { id: 4, task: 'Study', date: new Date() },
+//       { id: 5, task: 'Study', date: new Date() },
+//       { id: 6, task: 'Study', date: new Date() },
+//     ],
+//   },
+//   {
+//     name: 'Farhad',
+//     toDoes: [
+//       { id: 7, task: 'Study', date: new Date() },
+//       { id: 8, task: 'Study', date: new Date() },
+//       { id: 9, task: 'Study', date: new Date() },
+//     ],
+//   },
+// ];
+
+console.log(self);
+// function create() {
+//   var self = 'Me';
+// }
+{
+  var self = 'Me';
 }
+// create();
--- javascript/working-hours.js
+++ javascript/working-hours.js
@@ -1,40 +1,37 @@
-/**
- * Try the challenge with the Object.groupBy method
- *
- * @param {array} workingHours An array of working hours in the format provided at the end of file.
- *
- * @returns object with the comment format inside the function.
- */
-function workingHoursProxy(workingHours) {
-  // TODO: Implement here
-  //
-  //
-  // return {
-  //   "Saturday": ["9:00-14:00", "16:00-22:00"],
-  //   "Sunday": ["9:00-16:00"],
-  //   "Friday": ["9:00-22:00"]
-  // }
-}
+// Try the challenge with the Object.groupBy method
 
-// const workingHours = [
-//     {
-//       day_of_week: "Saturday",
-//       start_time: "9:00",
-//       end_time: "14:00",
-//     },
-//     {
-//       day_of_week: "Friday",
-//       start_time: "9:00",
-//       end_time: "16:00",
-//     },
-//     {
-//       day_of_week: "Saturday",
-//       start_time: "16:00",
-//       end_time: "22:00",
-//     },
-//     {
-//       day_of_week: "Sunday",
-//       start_time: "9:00",
-//       end_time: "22:00",
-//     },
-//    ];
+const workingHours = [
+  {
+    day_of_week: 'Saturday',
+    start_time: '9:00',
+    end_time: '14:00',
+  },
+  {
+    day_of_week: 'Friday',
+    start_time: '9:00',
+    end_time: '16:00',
+  },
+  {
+    day_of_week: 'Saturday',
+    start_time: '16:00',
+    end_time: '22:00',
+  },
+  {
+    day_of_week: 'Sunday',
+    start_time: '9:00',
+    end_time: '22:00',
+  },
+];
+
+function workingHoursProxy(workingHours) {
+  const newWorkingHours = Object.groupBy(
+    workingHours,
+    ({ day_of_week }) => day_of_week
+  );
+  for (let day in newWorkingHours) {
+    newWorkingHours[day] = newWorkingHours[day].map(
+      ({ start_time, end_time }) => `${start_time}-${end_time}`
+    );
+  }
+  return newWorkingHours;
+}
